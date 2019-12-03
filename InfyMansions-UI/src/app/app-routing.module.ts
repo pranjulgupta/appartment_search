174f8d4ebb6d801dd08d9349c1bf4ffce6d8f4d0
Suramyamol KS <suramyamol.ks@ad.infosys.com>
2019-12-03 18:08:52 +0530

    pushing buy component html

--- InfyMansions-UI/src/app/app-routing.module.ts
+++ InfyMansions-UI/src/app/app-routing.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './shared/login/login.component';
-<<<<<<< HEAD
-import { BuyComponent } from './shared/buy/buy.component';
-=======
 import { HomeComponent } from './shared/home/home.component';
->>>>>>> fae816170ec908f0561685c06f9d34769104a4f7
 const routes: Routes = [
 {
   path:"login",
   component:LoginComponent
 },
 {
-<<<<<<< HEAD
-  path:"buy",
-  component:BuyComponent
-=======
   path:"home",
   component:HomeComponent
->>>>>>> fae816170ec908f0561685c06f9d34769104a4f7
 },
 {
   path:"**", 
